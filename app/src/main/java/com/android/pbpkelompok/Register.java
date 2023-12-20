7c2573113f2f738272fe54f866928cd7f861440e
Hernansandi <hernanlaksono@gmail.com>
2023-12-20 22:49:39 +0700

    update main,register

--- app/src/main/java/com/android/pbpkelompok/Register.java
+++ app/src/main/java/com/android/pbpkelompok/Register.java
@@ -46,7 +46,7 @@ public class Register extends BottomSheetDialogFragment {
         db = new DataBaseHelperLogin(getActivity());
 
         // Set up the adapter and options for the roleSpinner
-        ArrayAdapter<String> roleAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, new String[]{"petugas dapur"});
+        ArrayAdapter<String> roleAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, new String[]{"Petugas Dapur"});
         roleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         roleSpinner.setAdapter(roleAdapter);
 
@@ -56,24 +56,50 @@ public class Register extends BottomSheetDialogFragment {
                 String inUsername = username.getText().toString();
                 String inPassword = password.getText().toString();
                 String inRePassword = repassword.getText().toString();
-                String inFullName = fullName.getText().toString(); // Get Full Name EditText value
-                String inRoleID = roleSpinner.getSelectedItem().toString(); // Get selected role from Spinner
-                String inStatus = "aktif"; // You can set a default value or remove this line if not needed
+                String inFullName = fullName.getText().toString();
+                String inRoleID = getRoleIDFromSpinner(roleSpinner.getSelectedItem().toString());
+                String inStatus = "aktif";
+
+                // Validasi input
+                if (inUsername.isEmpty() || inPassword.isEmpty() || inRePassword.isEmpty() || inFullName.isEmpty()) {
+                    Toast.makeText(getActivity(), "Semua inputan harus diisi", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 if (!inRePassword.equals(inPassword)) {
                     repassword.setError("Password Tidak Sama");
-                } else {
-                    // Updated method call to include Full Name, Role ID, and Status parameters
-                    Boolean daftarSuccess = db.simpanUser(inUsername, inPassword, inFullName, inRoleID, inStatus, null);
-                    if (daftarSuccess) {
-                        Toast.makeText(getActivity(), "Daftar Berhasil", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getActivity(), "Daftar Gagal", Toast.LENGTH_LONG).show();
-                    }
-                    dismiss();
+                    return;
                 }
+
+                // Validasi username sudah ada atau belum
+                if (checkUsername(inUsername)) {
+                    Toast.makeText(getActivity(), "Username sudah digunakan", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Updated method call to include Full Name, Role ID, and Status parameters
+                Boolean daftarSuccess = db.simpanUser(inUsername, inPassword, inFullName, inRoleID, inStatus, null);
+                if (daftarSuccess) {
+                    Toast.makeText(getActivity(), "Daftar Berhasil", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Daftar Gagal", Toast.LENGTH_LONG).show();
+                }
+                dismiss();
             }
         });
+    }
+
+    private String getRoleIDFromSpinner(String role) {
+        switch (role) {
+            case "Petugas Dapur":
+                return "E4";
+            default:
+                return "";
+        }
+    }
+
+    private boolean checkUsername(String username) {
+        return db.checkUsername(username);
     }
 
     @Override
